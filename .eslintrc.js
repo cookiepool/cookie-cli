7234c408b66514d9dfb95b7ab30f97a9a00a3d64
lihao <lihao@cmii.chinamobile.com>
2021-02-08 13:51:24 +0800

    增加eslint配置

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    node: true
+  },
+  extends: [],
+  parserOptions: {
+    'ecmaVersion': 7
+  },
+  globals: {},
+  rules: {
+    // 缩进使用2个空格
+    'indent': ['error', 2],
+    // 语句末尾始终使用分号
+    'semi': ['error', 'always'],
+    // 不允许声明了变量但是不使用，目前关闭函数参数里面的变量未使用不报错
+    'no-unused-vars': ['error', {
+      'vars': 'all',
+      'args': 'none',
+      'ignoreRestSiblings': false
+    }]
+  }
+};
